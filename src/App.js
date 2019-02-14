173495cc836ebabd1b1df0bfb92fc8008f38777f
Tom Porvaznik <tomporvaz@gmail.com>
2019-02-13 20:34:29 -0500

    added state, and event handlers.

--- src/App.js
+++ src/App.js
@@ -2,36 +2,64 @@ import React, { Component } from 'react';
 import marked from "marked";
 import './App.css';
 
-class MyMarkdown extends React.Component {
-  render () {
-    return <div>{marked('# Marked in browser\n\nRendered by **marked**.')}</div>
-  }
-}
 
+
+const myMarkdown = () => {
+  return {__html: marked('# Marked in browser\n\nRendered by **marked**.')};
+} 
+
+const sampleText = '# Marked in browser\n\nRendered by **marked**.';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      "input": sampleText,
+      "output": myMarkdown()
+    };
+    this.convert = this.convert.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  
+  convert() {
+    this.setState({
+      output: this.state.input 
+    })
+  }
+  
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+    console.log("Input: " + this.state.input);
+    console.log("Output:" + this.state.output);
+  }
+  
+  
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>Markdown Previewer</h1>
-          <div className="previewer">
-            <textarea id="editor"></textarea>
-            
-            <div id="preview"><MyMarkdown/></div>
-            
-          </div>
-
-        </header>
-        
-        
+      <header className="App-header">
+      <h1>Markdown Previewer</h1>
+      <div className="previewer">
+      <textarea id="editor" value={this.state.input} onChange={this.handleChange}></textarea>
+    
+      <div id="preview" dangerouslySetInnerHTML={this.state.output}></div>
+      
       </div>
-    );
+      
+      </header>
+      
+      
+      </div>
+      );
+    }
   }
-}
-
-
-export default App;
-
-
+  
+  
+  export default App;
+  
+  
+  
